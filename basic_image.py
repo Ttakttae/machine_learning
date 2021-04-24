3a0a24c314701874cac1bcf014ea52611e27bbd9
hyunwoo6321 <phw1508@naver.com>
2021-04-25 01:00:17 +0900

    Initial commit

---
+++ basic_image.py
@@ -0,0 +1,16 @@
+# tensorflow와 tf.keras를 임포트합니다
+import tensorflow as tf
+from tensorflow import keras
+
+# 헬퍼(helper) 라이브러리를 임포트합니다
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images.shape
