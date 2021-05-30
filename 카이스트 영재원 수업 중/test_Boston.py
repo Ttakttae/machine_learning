24e6d7886d4e36f7cccdb736be2419ea7af42100
hyunwoo6321 <phw1508@naver.com>
2021-05-30 11:43:39 +0900

    오류 수정

--- 카이스트 영재원 수업 중/test_Boston.py
+++ 카이스트 영재원 수업 중/test_Boston.py
@@ -3,7 +3,10 @@ import tensorflow as tf
 
 def test_machine_learning():
     data = pd.read_csv('카이스트 영재원 수업 중/boston.csv')
-    독립 = 보스턴[['crim', 'zn']]
-    
+    print(data.shape)
+    print(data.columns)
+    x_data = data[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax', 'ptratio', 'b', 'lstat']]
+    y_data = data[['medv']]
+    print(x_data.shape, y_data.shape)
 
 test_machine_learning()
