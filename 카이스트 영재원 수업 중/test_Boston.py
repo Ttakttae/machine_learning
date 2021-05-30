351b2dcbe2e690e0a597f0a046239eb5e7901569
hyunwoo6321 <phw1508@naver.com>
2021-05-30 11:49:10 +0900

    중간 저장

--- 카이스트 영재원 수업 중/test_Boston.py
+++ 카이스트 영재원 수업 중/test_Boston.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import tensorflow as tf
+from tensorflow.python.util.nest import _yield_value
 
 def test_machine_learning():
     data = pd.read_csv('카이스트 영재원 수업 중/boston.csv')
@@ -8,5 +9,10 @@ def test_machine_learning():
     x_data = data[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax', 'ptratio', 'b', 'lstat']]
     y_data = data[['medv']]
     print(x_data.shape, y_data.shape)
+    X = tf.keras.layers.Input(shape=[13])
+    Y = tf.keras.layers.Dense(1)(X)
+    model = tf.keras.models.Model(X, Y)
+    model.compile(loss='mse')
+    model.fit(x_data, y_data, epochs = 10000)
 
 test_machine_learning()
