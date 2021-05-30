08e281e06edb7f48d6766a78f7e7a964799ce595
hyunwoo6321 <phw1508@naver.com>
2021-05-30 11:25:15 +0900

    중간저장

--- 카이스트 영재원 수업 중/practicing.py
+++ 카이스트 영재원 수업 중/practicing.py
@@ -1,27 +1,30 @@
 import pandas as pd
 import tensorflow as tf
 
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/lemonade.csv'
-레모네이드 = pd.read_csv(파일경로)
-독립 = 레모네이드[['온도']]
-종속 = 레모네이드[['판매량']]
-print(레모네이드)
+def test_machine_learning():
+    파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/lemonade.csv'
+    레모네이드 = pd.read_csv(파일경로)
+    독립 = 레모네이드[['온도']]
+    종속 = 레모네이드[['판매량']]
+    print(레모네이드.shape)
 
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
-보스턴 = pd.read_csv(파일경로)
-print(보스턴)
+    파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
+    보스턴 = pd.read_csv(파일경로)
+    print(보스턴)
 
-파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv'
-아이리스 = pd.read_csv(파일경로)
-print(아이리스)
+    파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv'
+    아이리스 = pd.read_csv(파일경로)
+    print(아이리스)
 
-X = tf.keras.layers.Input(shape=[1])
-Y = tf.keras.layers.Dense(1)(X)
-model = tf.keras.models.Model(X,Y)
-model.compile(loss='mse')
+    X = tf.keras.layers.Input(shape=[1])
+    Y = tf.keras.layers.Dense(1)(X)
+    model = tf.keras.models.Model(X,Y)
+    model.compile(loss='mse')
 
-model.fit(독립, 종속, epochs=10000)
+    model.fit(독립, 종속, epochs=10000)
 
-model.predict(독립)
+    model.predict(독립)
 
-print(독립)
+    print(독립)
+
+test_machine_learning()
