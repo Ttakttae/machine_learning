9a232bcffdd8f103323c45624682da5cec06ec6d
hyunwoo6321 <phw1508@naver.com>
2021-05-30 11:36:00 +0900

    오류 수정

--- 카이스트 영재원 수업 중/test_Boston.py
+++ 카이스트 영재원 수업 중/test_Boston.py
@@ -2,7 +2,7 @@ import pandas as pd
 import tensorflow as tf
 
 def test_machine_learning():
-    보스턴 = pd.read_csv('카이스트 영재원 수업 중/boston.csv')
+    data = pd.read_csv('카이스트 영재원 수업 중/boston.csv')
     독립 = 보스턴[['crim', 'zn']]
     
 
