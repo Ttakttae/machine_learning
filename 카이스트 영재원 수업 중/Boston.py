330f53afcb0d6399313cb72f77cf96b216fb98b6
hyunwoo6321 <phw1508@naver.com>
2021-05-30 11:25:04 +0900

    중간 저장

---
+++ 카이스트 영재원 수업 중/Boston.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import tensorflow as tf
+
+def test_machine_learning():
+    보스턴 = pd.read_csv('카이스트 영재원 수업 중/boston.csv')
+    독립 = 보스턴[['crim', 'zn']]
+
+
+test_machine_learning()
